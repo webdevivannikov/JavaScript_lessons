972a96c07ee9830ef9cfb50b483d08fe5ac74f7d
Sanek <polkil0@yandex.ru>
2018-08-15 02:13:15 +0700

    lesson_1_1

--- lesson_1/simple/script.js
+++ lesson_1/simple/script.js
@@ -1,21 +1,14 @@
-var acc = +prompt("Ваш бюджет на месяц:", "30");
-var nameShop = prompt("Название вашего магазина:", "Магазин");
-
-var mainList = {}
-var staff = new Object();
-
-mainList = {
-	account: 12,
-	shopName: "name",
-	shopGoods: ['plum'],
-	open() {
-		return false;
-	} 
+var myAccount = +prompt("Ваш бюджет на месяц:", "30");
+	shopName = prompt("Название вашего магазина:", "Магазин");
+	mainList = {
+		account: myAccount,
+		nameShop: shopName,
+		shopGoods: [],
+		employers: {},
+		open: false
+	}
+for (let i = 0; i < 3; i++){
+	mainList.shopGoods[i] = prompt("Какой тип товаров будем продавать?");
 }
 
-mainList.account = acc;
-mainList.shopName = nameShop;
-mainList.shopGoods[0] = prompt("Какой тип товаров будем продавать?", "Апельсины");
-mainList.shopGoods[1] = prompt("Какой тип товаров будем продавать?", "Мандарины");
-mainList.shopGoods[2] = prompt("Какой тип товаров будем продавать?", "Арбузы");
 console.log("бюджет на 1 день - " + mainList.account/30 + " тыс. руб.");
