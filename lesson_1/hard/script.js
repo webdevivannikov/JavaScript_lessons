59a7eed635c5add7e3b2d4561d85537aa5495811
Sanek <polkil0@yandex.ru>
2018-08-15 13:34:46 +0700

    lesson_1_2

--- lesson_1/hard/script.js
+++ lesson_1/hard/script.js
@@ -1,6 +1,21 @@
-var num = 33721;
-var string = Math.pow(num*num, 3);
-string = new String(string);
-console.log(num*num);
+var number = 33721;
 
-console.log(string.charAt(0) + string.charAt(2));
+console.log(number);
+function opNumbers(num) {
+	var result = 1;
+	if (isNaN(num)) {
+	  alert( "Строка преобразовалась в NaN. Не число" );
+	  return "ошибка";
+	} 
+	else {
+		while (num != 0){
+			result *= num % 10;
+			num = Math.floor(num / 10);  
+		}
+	  return result;
+	}	
+};
+console.log(opNumbers(number));
+console.log(Math.pow(opNumbers(number),3));
+var string = new String(Math.pow(opNumbers(number),3));
+console.log(string.charAt(0) + string.charAt(1));
