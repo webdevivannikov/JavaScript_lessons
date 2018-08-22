6f464a72c0162b7d144df5257e9d7b1d59f5a19f
Sanek <polkil0@yandex.ru>
2018-08-23 02:40:30 +0700

    lection1

---
+++ lection_1/js/script.js
@@ -0,0 +1,12 @@
+for (let i = 2; i < 101; i++){
+	rez = true;
+	for (let j = 2; j < Math.sqrt(i); j++){
+		if (i % j == 0) {
+			rez = false; 
+			break
+		}
+		if (rez) {
+			console.log(i + " - Делители этого числа: 1 и " + i);
+		}
+	}
+}
