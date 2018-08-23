7db6b2d5cc823c857f355b5943297269eca05431
Sanek <polkil0@yandex.ru>
2018-08-23 23:42:06 +0700

    lesson5

--- lection_1/js/script.js
+++ lection_1/js/script.js
@@ -1,12 +1,13 @@
 for (let i = 2; i < 101; i++){
-	rez = true;
-	for (let j = 2; j < Math.sqrt(i); j++){
+	let rez = true;
+	for (let j = 2; j <= Math.sqrt(i); j++){
 		if (i % j == 0) {
 			rez = false; 
-			break
-		}
-		if (rez) {
-			console.log(i + " - Делители этого числа: 1 и " + i);
+			break;
 		}
 	}
+	if (rez) {
+			console.log(i + " - Делители этого числа: 1 и " + i);
+	}
+	
 }
