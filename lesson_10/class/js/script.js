f3267149bc31781704ec2d8221e6d514eb3bbcbc
Sanek <polkil0@yandex.ru>
2018-08-30 01:43:00 +0700

    lesson10_1

--- lesson_10/class/js/script.js
+++ lesson_10/class/js/script.js
@@ -1,20 +1,26 @@
 class options {
-	constructor(height, width, bg){
+	constructor(height, width, bg, fontSize, textAlign){
 		this.height = height;
 		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.textAlign = textAlign;
 	}
 
 	createDiv(text) {
 		let div = document.createElement('div');
 		div.innerHTML = text;
-		div.style.cssText = `height: $(this.height);
-		width: $(this.width)
+		div.style.cssText = `height: ${this.height};
+		width: ${this.width};
+		background-color: ${this.bg};
+		font-size: ${this.fontSize};
+		text-align: ${this.textAlign}
 		`;
 		document.body.appendChild(div);
 	}
  }
 
- const newDiv = new options("200px","150px");
+ const newDiv = new options("200px", "150px", "red", "18px", "center");
 
  newDiv.createDiv("Новый Div");
 
