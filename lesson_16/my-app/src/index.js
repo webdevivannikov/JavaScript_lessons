bc6c37f1650ed5b470bbb43425a0641ab5577688
Sanek <polkil0@yandex.ru>
2018-09-05 22:57:22 +0700

    lesson16

---
+++ lesson_16/my-app/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Clock from './App';
+import Button from './button';
+import registerServiceWorker from './registerServiceWorker';
+
+class App extends React.Component {
+	render() {
+		return (
+				<div className="wrapper">
+					<Clock />
+					<Button />
+				</div>
+			)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
